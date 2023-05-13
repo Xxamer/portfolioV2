4ba3d1be33b47f3146f6af70fbc2cfe03ca3c20d
Christian Granado Rodriguez <christian.g@smartwaystudio.com>
2023-05-13 14:31:54 +0100

    Added things to test

--- src/components/cyberpunk/CyberpunkHeader.vue
+++ src/components/cyberpunk/CyberpunkHeader.vue
@@ -1,8 +1,12 @@
 <template>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Orbitron&display=swap">
     <div class="cyberheader">
+        <div  style="color:white" class="">
+               <img class="profile-img glitch" src="https://avatars.githubusercontent.com/u/36738325?v=4" alt="">
+            </div>
         <div class="container">
-            <div class="glitch" data-text="CHRISTIAN GRANADO">CHRISTIAN GRANADO</div>
+           
+            <div class="glitch-mobile " data-text="CHRISTIAN GRANADO">CHRISTIAN GRANADO</div>
             <div class="glow">CHRISTIAN GRANADO</div>
             <p class="subtitle">CYBER DEVELOPER</p>
         </div>
--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -2,6 +2,16 @@
     <main>
         <div v-if="cyberpunked">
             <CyberpunkHeader title="Callate por favor me esta dando un pabajo" />
+            <button class="cybr-btn">
+                Cyber<span aria-hidden>_</span>
+                <span class="cybr-btn__glitch">Cyber_</span>
+                <span class="cybr-btn__tag">R25</span>
+            </button>
+            <button class="cybr-btn">
+                Buttons<span aria-hidden>_</span>
+                <span class="cybr-btn__glitch">Buttons_</span>
+                <span class="cybr-btn__tag">R25</span>
+            </button>
         </div>
         <div v-else>
             Modo synthwawe, aqui no hay nada
@@ -13,11 +23,14 @@ import { onMounted, ref } from "vue";
 import cyberpunk from '@/interfaces/cyberpunk';
 import CyberpunkHeader from "@/components/cyberpunk/CyberpunkHeader.vue";
 
-const hasBeen:cyberpunk = { cyberpunked: ref(true) };
-let { cyberpunked } = hasBeen; // ✅ name es reactivo
+const hasBeen: cyberpunk = { cyberpunked: ref(true) };
+
+let { cyberpunked } = hasBeen;
+const keySequence: any = [];
 
 onMounted(() => {
-    const probabilidadTrue = 0.01;
+
+    const probabilidadTrue = 1;
     const random = Math.random();
     hasBeen.cyberpunked.value = random <= probabilidadTrue ? true : false;
 })
